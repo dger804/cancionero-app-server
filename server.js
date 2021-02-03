2677098f008b95b381206c5d6340d5b23ca39c39
dger804 <diego804@gmail.com>
2021-02-03 15:13:23 -0500

    Se coloca la ruta al server de usuarios

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const { connect } = require('./src/db.js');
+const userRouter = require('./src/routes/user')
 
 const port = process.env.PORT;
 const app = express();
@@ -9,6 +10,8 @@ connect();
 
 app.use(express.json())
 app.use(cors())
+
+app.use('/users', userRouter);
 
 app.listen(port, () => {
     console.log(`Servidor escuchando por http://localhost:${port}`)
