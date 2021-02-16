9cae106dd0631d1af34215bc0bcd2b6bc2f5ee47
dger804 <diego804@gmail.com>
2021-02-16 17:06:17 -0500

    Se quita autenticación para el método listar

--- src/routes/folder.js
+++ src/routes/folder.js
@@ -3,6 +3,6 @@ const folderController = require('../controllers/folder.controller');
 const { auth } = require('../utils/auth');
 
 router.route('/').post(auth, folderController.create);
-router.route('/').get(auth, folderController.list);
+router.route('/').get(folderController.list);
 
 module.exports = router;
