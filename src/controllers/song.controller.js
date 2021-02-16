37c49ab49fa017a4ea6a652fc3cd4d084fbca681
dger804 <diego804@gmail.com>
2021-02-16 17:02:10 -0500

    Se envía a las canciones de folder la cancion creada y se deja funcionando el método para listar todas las canciones

--- src/controllers/song.controller.js
+++ src/controllers/song.controller.js
@@ -1,33 +1,40 @@
-const User = require('../models/user.model');
 const Folder = require('../models/folder.model');
 const Song = require('../models/song.model');
 
 module.exports = {
-  async list(req, res){
-    try{
-      const song = await Song.find().populate(
-        'userId, folderId'
-      ).catch(err => { res.status(500).json(err) })
-    }catch(err){
-      res.status(400).json({ message: err.message });
-    }
-  },
   async create(req, res){
     try{
-      const userId = req.userId;
-      const user = await User.findById( userId );      
-      const { Folderid, titulo, artista, tono, letra } = req.body;
+      const folderId = req.body;  
+      const id = folderId.carpetaid;
+      const folder = await Folder.findById( id );
+      if(!folder){
+        throw new Error('Carpeta inválida')
+      }    
+      const { carpetaid, titulo, artista, tono, letra } = req.body;
       const song = await Song.create(
         { 
-          Folderid: user.carpetas,
+          carpetaid: id,
           titulo,
           artista,
           tono,
           letra,
         }
       ); 
+      folder.canciones.unshift( song );
+      await folder.save({ validateBeforeSave: false });
+      res.status(201).json({ message: 'Canción creada', data: song });
     }catch(err){
       res.status(400).json({ message: err.message })
     }
-  }
+  },
+  async list(req, res){
+    try{
+      const songs = await Song.find().catch(
+        err => { res.status(500).json(err) }
+      )
+      res.status(200).json({ message: "Canciones encontradas", data: songs })
+    }catch(err){
+      res.status(400).json({ message: err.message });
+    }
+  },
 }
